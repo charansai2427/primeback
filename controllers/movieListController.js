64504f5f8ac03fe959facfe819aaa08bb085925a
charansai2427 <131942169+charansai2427@users.noreply.github.com>
2023-09-26 15:33:57 +0530

    commit

--- controllers/movieListController.js
+++ controllers/movieListController.js
@@ -1,12 +1,14 @@
 const {Movies} =require("../mongoConfig");
-const MovieModel = require("../model/movieListModel");
+const {ObjectId}= require("mongodb")
 
 const addMovie =(req) =>{
     return Movies.insertOne(req.body);
 }
 
 const getMovie =(req) =>{
-    return Movies.findOne(req.body.userId);
+    const id = new ObjectId(req.params.movieId)
+    console.log(id);
+    return Movies.findOne({_id:req.params.movieId});
 }
 
 const getAllMovies = (req) =>{
